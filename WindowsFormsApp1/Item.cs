4f8fab9b4d4dd1120d9a9748f60b169347c7f3dc
alphaomega325 <alphaomega325@gmail.com>
2019-04-06 14:38:07 -0500

    Added the Item and person class

---
+++ WindowsFormsApp1/Item.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Retalo
+{
+
+    public class Item
+    {
+
+	public string Name{
+	    get;
+	}
+	
+	public int ID{
+	    get;
+	}
+
+    }
+
+}
---
+++ WindowsFormsApp1/Person.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Retalo
+{
+
+    public class Person : Item
+    {
+
+        private int reward_point;
+
+        public Boolean IsTeacher {
+	        get;
+	    }
+
+	    public Boolean IsVeteran {
+	        get;
+	    }
+
+	    public Boolean IsSeniorCitizen {
+	        get;
+	    }
+
+	    public int Reward_Point {
+	        get
+	        {
+		    return reward_point;
+	        }
+	        private set
+	        {
+                reward_point = value;
+	        }
+	    }
+
+
+
+    }
+
+}
