78fa06f568679d489299f1e3b636d5b204a463d2
alphaomega325 <alphaomega325@gmail.com>
2019-05-07 10:58:04 -0500

    Fixed Delete Item

--- WindowsFormsApp1/Database/UniversalDatabaseOperations.cs
+++ WindowsFormsApp1/Database/UniversalDatabaseOperations.cs
@@ -7,34 +7,34 @@ namespace Retalo
 
     public static class DatabaseOperation
     {
-/*
-        public static SqlCommand Return_Sql_Select_String(string database, int id, string idname, SqlConnection connect){
+        /*
+                public static SqlCommand Return_Sql_Select_String(string database, int id, string idname, SqlConnection connect){
 
 
-            string selectStatement =
-                "Select *"
-                + "FROM " + database 
-                + " Where @idname = @id";
-            SqlCommand selectCommand = new SqlCommand(selectStatement, connect);
+                    string selectStatement =
+                        "Select *"
+                        + "FROM " + database 
+                        + " Where @idname = @id";
+                    SqlCommand selectCommand = new SqlCommand(selectStatement, connect);
 
-            //selectCommand.Parameters.AddWithValue("@database", database);
-            selectCommand.Parameters.AddWithValue("@id", id);
-            selectCommand.Parameters.AddWithValue("@idname", idname);
-            return selectCommand;
+                    //selectCommand.Parameters.AddWithValue("@database", database);
+                    selectCommand.Parameters.AddWithValue("@id", id);
+                    selectCommand.Parameters.AddWithValue("@idname", idname);
+                    return selectCommand;
 
-        }
-        
-    */
+                }
+
+            */
         public static Person ReturnItem(Person person)
         {
-            if(person.ID.ToString() == "")
+            if (person.ID.ToString() == "")
             {
                 person = null;
                 return person;
 
             }
 
-            try{
+            try {
                 SqlConnection connect = RetaloDB.GetConnection();
 
                 string selectStatement =
@@ -47,17 +47,17 @@ namespace Retalo
                 Person person2 = PersonOperations.ReturnPerson(selectCommand, connect);
                 return person2;
             }
-            catch(Exception ex){
+            catch (Exception ex) {
                 throw ex;
             }
-            
 
-            
+
+
         }
 
         public static Product ReturnItem(Product product)
         {
-            try{
+            try {
                 SqlConnection connect = RetaloDB.GetConnection();
 
 
@@ -68,52 +68,72 @@ namespace Retalo
                 SqlCommand selectCommand = new SqlCommand(selectStatement, connect);
                 selectCommand.Parameters.AddWithValue("@id", product.ID);
 
-               
-               // selectCommand = Return_Sql_Select_String("Product", product.ID, "ProdID", connect);
+
+                // selectCommand = Return_Sql_Select_String("Product", product.ID, "ProdID", connect);
                 Product product2 = ProductOperations.ReturnProduct(selectCommand, connect);
                 return product2;
             }
-            catch(Exception ex){
+            catch (Exception ex) {
                 throw ex;
             }
-            
 
 
-            
+
+
         }
 
-        
-        public static Boolean DeleteItem(int id, String database){
-            
-            //Delete Item Object Parser and check if item exists
 
-            if(database == "Person")
+        public static Boolean DeleteItem(int id, String database) {
+
+            //Delete Item Object Parser and check if item exists
+            SqlConnection connection = RetaloDB.GetConnection();
+            SqlCommand remove;
+            if (database == "Person")
             {
 
                 Person person = new Person();
                 person.ID = id;
                 if (ReturnItem(person) == null)
                 {
+                    connection.Close();
                     return false;
 
                 }
 
+                string removestatement =
+                "DELETE FROM " + database + " WHERE PerID = @id";
+                remove = new SqlCommand(removestatement, connection);
+                remove.Parameters.AddWithValue("@id", id);
+
+                return DeleteItemAction(remove, connection);
+
+
             }
-            
- 
+
+
+
+            else
+            {
+                return false;
+            }
+            /*
             SqlConnection connection = RetaloDB.GetConnection();
             string removestatement = 
                 "DELETE FROM " + database +" WHERE @idname = @id";
             SqlCommand remove = new SqlCommand(removestatement, connection);
             remove.Parameters.AddWithValue("@id", id);
             //remove.Parameters.AddWithValue("@database", database);
-            if(database == "Person"){
+                
+    if(database == "Person"){
                   remove.Parameters.AddWithValue("@idname", "PerID");        
             }
             else{
                 return false;
             }
-                
+        */
+        }
+        private static Boolean DeleteItemAction(SqlCommand remove, SqlConnection connection)
+        {
             try
             {
                 connection.Open();
@@ -128,11 +148,13 @@ namespace Retalo
             finally
             {
                 connection.Close();
-                    
+
             }
             return true;
 
         }
+    
+
 
         public static Boolean AddorUpdateItem(Person person)
         {
